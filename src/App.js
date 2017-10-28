862939604bd25371a99b94537b8412beadf39bda
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2017-10-28 13:51:15 +0200

    Bootstrap interface

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            answer: {
+                answer: "no",
+                forced: false,
+                image: "https://yesno.wtf/assets/no/0-b6d3e555af2c09094def76cf2fbddf46.gif"
+            },
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="question">
+                    <input type="text" />
+                    <button type="submit">Ask to the Gods of the Internet!</button>
+                </div>
+
+                {this.state.answer && (
+                    <div className="answer">
+                        <h1>{this.state.answer.answer}</h1>
+                        <img src={this.state.answer.image} alt={this.state.answer.answer} />
+                    </div>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('should display a text input to fill question', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('input[type="text"]').length).toBe(1);
+});
+
+it('should display a submit button to send question to the Internet Gods', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('button[type="submit"]').length).toBe(1);
+});
+
+it('should not display any answer by default', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.answer').length).toBe(0);
+});
+
+it('should display Internet Gods answer in both text and picture format', () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({
+        answer: {
+            answer: 'no',
+            forced: false,
+            image: 'https://yesno.wtf/assets/no/0-b6d3e555af2c09094def76cf2fbddf46.gif',
+        },
+    });
+
+    const answer = wrapper.find('.answer');
+    expect(answer.find('h1').text()).toBe('no');
+    expect(answer.find('img').prop('src')).toBe('https://yesno.wtf/assets/no/0-b6d3e555af2c09094def76cf2fbddf46.gif');
 });
